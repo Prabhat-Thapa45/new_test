ef5d422dc98c3c6b61e6a24c38451cd7edae20c1
Prabhat Thapa <akoup66@gmail.com>
2021-10-12 15:25:20 +0545

    Update test_validate_input.py

--- test/test_validate_input.py
+++ test/test_validate_input.py
@@ -1,29 +1,21 @@
-""" this module tests validate_input.py """
-
-import pytest
-from src.utility.validate_input import validate_int, validate_float
+""" this module tests welcome_api.py """
 
 
-@pytest.fixture
-def test_data():
-    return [1, 3], [-1, "as", 0]
+def test_index(welcome_client, welcome_urls):
+    response = welcome_client.get(welcome_urls['index'])
+    assert response.status_code == 200
 
 
-def test_validate_int(test_data):
-    for i in test_data[0]:
-        assert validate_int(i) == i
+def test_about(welcome_urls, welcome_client):
+    response = welcome_client.get(welcome_urls['about'])
+    assert response.status_code == 200
 
 
-def test_validate_int_negative(test_data):
-    for i in test_data[1]:
-        assert not validate_int(i)
+def test_contact(welcome_urls, welcome_client):
+    response = welcome_client.get(welcome_urls['contact'])
+    assert response.status_code == 200
 
 
-def test_validate_float(test_data):
-    for i in test_data[0]:
-        assert validate_float(i) == i
-
-
-def test_validate_float_negative(test_data):
-    for i in test_data[1]:
-        assert not validate_float(i)
+def test_menu(welcome_urls, welcome_client):
+    response = welcome_client.get(welcome_urls['menu'])
+    assert response.status_code == 200
